b72f008b9f8293dbe27ba185862e187467aa2260
Henry Hazan <henry@teramine.io>
2024-02-13 10:57:00 +0200

    organize docs

--- MIL/C01_Introduction/solutions/Solutions_S02_Overview.lean
+++ MIL/C01_Introduction/solutions/Solutions_S02_Overview.lean
@@ -4,4 +4,13 @@ import MIL.Common
 
 open Nat
 
+
+
+
+
+theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
+fun hpq : p ∧ q =>
+have hp : p := And.left hpq
+have hq : q := And.right hpq
+show q ∧ p from And.intro hq hp
 -- There are no exercises in this section.
